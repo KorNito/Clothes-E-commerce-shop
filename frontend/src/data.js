36e63c180cdcea5c560cdd066f536e561677ba88
KorNito <kornelijuspacevicius@gmail.com>
2020-11-10 01:18:30 +0200

    Added available clothes

---
+++ frontend/src/data.js
@@ -0,0 +1,29 @@
+export default {
+    products: [{
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/slim-shirt.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        reviews: 10
+    },
+    {
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/fit-shirt.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.2,
+        reviews: 10
+    },
+    {
+        name: 'Kelnės',
+        category: 'Pants',
+        image: '/images/pants.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        reviews: 8
+    }],
+}
