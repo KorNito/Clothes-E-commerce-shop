bedba874e2dd895a8204b25a1cba5cd8da84a365
KorNito <kornelijuspacevicius@gmail.com>
2020-11-10 02:43:33 +0200

    Initial backend commit

---
+++ backend/data.js
@@ -0,0 +1,32 @@
+export default {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/slim-shirt.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        reviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/fit-shirt.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.2,
+        reviews: 10
+    },
+    {
+        _id: '3',
+        name: 'Kelnės',
+        category: 'Pants',
+        image: '/images/pants.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        reviews: 8
+    }],
+}
